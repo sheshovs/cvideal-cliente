f32207b0c5f650cbeee1f9c022a207feda2864c7
sergioupbirk <sergio.vargas@upbirk.com>
2022-05-19 23:06:09 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Main, Login, Profile, Projects, Services } from "./pages/public";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="login" element={<Login />} />
+				<Route path="profile" element={<Profile />} />
+				<Route path="projects" element={<Projects />} />
+				<Route path="services" element={<Services />} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+				<Route path="*" element={<Navigate to="/" replace />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
---
+++ src/pages/public/index.js
@@ -0,0 +1,5 @@
+export { default as Main } from "./Main.js";
+export { default as Login } from "./Login.js";
+export { default as Profile } from "./Profile.js";
+export { default as Projects } from "./Projects.js";
+export { default as Services } from "./Services.js";
